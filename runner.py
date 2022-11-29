36a94282b98cb34732938219cd8a50232d877802
jiazifa <2332532718@qq.com>
2022-11-29 22:37:04 +0800

    修复入口

--- runner.py
+++ runner.py
@@ -2,5 +2,4 @@ from cartoon.cli import main as main_func, any_download_playlist
 import sys
 
 if __name__ == "__main__":
-    # main_func(sys.kwargs)
-    any_download_playlist("http://www.177picyy.com/html/category/tt/page/1")
+    main_func(sys.kwargs)
